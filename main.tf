9aad6a0e8893376b96415d9640e833a127b250e0
kunal-bodke <109844542+kunal-bodke@users.noreply.github.com>
2022-12-30 16:48:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-	resource "local_file" "devops" {
+resource "local_file" "devops" {
 	filename = var.file_list[0]
 	content = var.content_map["content1"]
 }
 
-	resource "local_file" "devops_2nd" {
+resource "local_file" "devops_2nd" {
 	filename = var.file_list[1]
 	content = var.content_map["content2"]
 }
